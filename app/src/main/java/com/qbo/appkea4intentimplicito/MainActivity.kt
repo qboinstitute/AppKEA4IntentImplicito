0faa22341be5ff1cda786541e31182dd0bed83df
LuisAngel <luisangel.salvat@gmail.com>
2021-04-08 18:52:37 -0500

    Intent Implicito 08042021

--- app/src/main/java/com/qbo/appkea4intentimplicito/MainActivity.kt
+++ app/src/main/java/com/qbo/appkea4intentimplicito/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.qbo.appkea4intentimplicito
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.qbo.appkea4intentimplicito.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btncompartir.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        val enviarIntent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, "Hola mundo")
+            type = "text/plain"
+        }
+        if(enviarIntent.resolveActivity(packageManager) != null){
+            startActivity(enviarIntent)
+        }
     }
 }
